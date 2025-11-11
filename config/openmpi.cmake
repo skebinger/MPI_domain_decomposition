681edb176481e24c48a36ff64cedb57dd08c641e
skebinger <simon.kebinger@tugraz.at>
2025-11-11 13:08:24 +0100

    added some gofrtran flags for compiling with the openmpi option - mainly optimization flags. also added free line length since some systems complained while testing

--- config/openmpi.cmake
+++ config/openmpi.cmake
@@ -27,13 +27,14 @@ else()
         -Wall
         -Wextra
         -Wno-c-binding-type
+	    -ffree-line-length-0
     )
 endif()
 
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
     set(MPI_OPT_FLAGS -O0 -g -fbounds-check -fbacktrace)
 else()
-    set(MPI_OPT_FLAGS -O3)
+    set(MPI_OPT_FLAGS -O3 -funroll-loops -ftree-vectorize -flto=3 -march=native -finline-functions -fimplicit-none)
 endif()
 
 set(MPI_COMPILE_FLAGS ${MPI_BASE_FLAGS} ${MPI_OPT_FLAGS})
